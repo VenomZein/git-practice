57feb03fa61f6aa091e0d5a047303f62f0009c2b
VenomZein <zeinabidinmedval@gmail.com>
2026-07-24 12:08:05 +0100

    add celcius

--- hello.py
+++ hello.py
@@ -4,3 +4,8 @@ print('this is my first git commit')
 print('edited on github')
 
 print('this only exists on the branch')
+
+def celsius_to_fahrenheit(c):
+    return c * 9/5 + 32
+
+print(celsius_to_fahrenheit(25))
